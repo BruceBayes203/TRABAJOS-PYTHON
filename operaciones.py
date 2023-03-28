a8563ce5e0301feb788d56ff778c149c5aa975e1
brucebayes203 <estibengarate@gmail.com>
2023-03-28 09:01:41 -0500

    v5

---
+++ operaciones.py
@@ -0,0 +1,9 @@
+c=int(input("ingrese numero1"))
+a=int(input("ingrese numero2"))
+b=int(input("ingrese numero3"))
+sum=c+a+b
+print("suma es",sum)
+multiplicacion=c*a*b
+print("la multiplicacion es",multiplicacion)
+division=c+a/b
+print("ladivision es",division)
---
+++ triangulo.py
@@ -0,0 +1,4 @@
+base=float(input("ingresela base"))
+altura=float(input("ingresela altura"))
+area=(base*altura)/2
+print("el area es",area)
